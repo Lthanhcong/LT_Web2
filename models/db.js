e692e9d9b1878ee806301f5589b36dbec75fbf22
Le Cong <congle00089@gmail.com>
2021-07-21 14:13:19 +0700

    update

--- models/db.js
+++ models/db.js
@@ -1,4 +1,24 @@
-const { Sequelize } = require("sequelize");
-module.exports = new Sequelize(
-  process.env.DATABASE_URL || "postgres://postgres:Lethanhcong2401@localhost:5432/LTWEB2"
-);
+const { Sequelize } = require('sequelize');
+
+module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Lethanhcong2401@localhost:5432/LTWEB2',{
+  // dialect: 'postgres',
+  // protocol: 'postgres',
+  // dialectOptions: {}, //removed ssl
+  dialect:'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    useUTC: false,
+    dateStrings: false,
+    typeCast: function (field, next) { // for reading from database
+      if (field.type === 'DATETIME') {
+        return field.string();
+      }
+      return next();
+    },
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  timezone: 'Asia/Ho_Chi_Minh'
+});
