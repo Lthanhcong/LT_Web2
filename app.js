2b9efa9d5d9b516d67bb56801a6744857cd49ef9
thanvanductinh <18600278@student.hcmus.edu.vn>
2021-08-28 11:22:32 +0700

    Search

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+var bodyParser = require("body-parser");
 // const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
-const db = require('./models/db');
+const path = require("path");
+const db = require("./models/db");
 
-const authMiddleware = require('./middlewares/auth')
+const authMiddleware = require("./middlewares/auth");
 
-var cookieSession = require('cookie-session');
+var cookieSession = require("cookie-session");
 
 // const adminRouter = require('./routers/admin');
 // const userRouter = require('./routers/user');
 // const authRouter = require('./routers/auth');
-const indexRouter = require('./routers/index');
-const adminRouter = require('./routers/admin');
-const authRouter = require('./routers/auth');
-const categoryRouter = require('./routers/category');
-const defaultRouter = require('./routers/default');
-const phimRouter = require('./routers/phim');
+const indexRouter = require("./routers/index");
+const adminRouter = require("./routers/admin");
+const authRouter = require("./routers/auth");
+const categoryRouter = require("./routers/category");
+const defaultRouter = require("./routers/default");
+const phimRouter = require("./routers/phim");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 // app.use(expressLayouts);
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // app.use(Middlewares);
 
 //Session
-app.use(cookieSession({
-    name: 'session',
-    keys: [process.env.COOKIE_KEY || 'secret'],
+app.use(
+    cookieSession({
+        name: "session",
+        keys: [process.env.COOKIE_KEY || "secret"],
 
-    //Cookie Options
-    maxAge: 24 * 60 * 60 * 1000,
-}))
+        //Cookie Options
+        maxAge: 24 * 60 * 60 * 1000,
+    })
+);
 
 app.use(authMiddleware);
 
 //EJS
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 // app.set('views', path.join(__dirname, 'views'));
 // app.use('/assets', express.static(__dirname + '/public'));
-app.use('/assets', express.static('assets'))
+app.use("/assets", express.static("assets"));
 
 //Xử lý chức năng đăng nhập của cả admin và user
 // //controller
@@ -60,8 +62,10 @@ app.get("/", async function(req, res) {
     res.redirect("/default");
 });
 
-db.sync().then(function() {
-    const port = process.env.PORT || 3000;
-    console.log(`Server is listening on port ${port}`);
-    app.listen(port);
-}).catch(console.error)
+db.sync()
+    .then(function() {
+        const port = process.env.PORT || 3000;
+        console.log(`Server is listening on port ${port}`);
+        app.listen(port);
+    })
+    .catch(console.error);
--- models/db.js
+++ models/db.js
@@ -1,24 +1,26 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Lethanhcong2401@localhost:5432/LTWEB2',{
-  // dialect: 'postgres',
-  // protocol: 'postgres',
-  // dialectOptions: {}, //removed ssl
-  dialect:'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    useUTC: false,
-    dateStrings: false,
-    typeCast: function (field, next) { // for reading from database
-      if (field.type === 'DATETIME') {
-        return field.string();
-      }
-      return next();
-    },
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  timezone: 'Asia/Ho_Chi_Minh'
-});
+module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:pl0966257564@localhost:5432/Cinema',
+    //{
+    //     // dialect: 'postgres',
+    //     // protocol: 'postgres',
+    //     // dialectOptions: {}, //removed ssl
+    //     dialect: 'postgres',
+    //     protocol: 'postgres',
+    //     dialectOptions: {
+    //         useUTC: false,
+    //         dateStrings: false,
+    //         typeCast: function(field, next) { // for reading from database
+    //             if (field.type === 'DATETIME') {
+    //                 return field.string();
+    //             }
+    //             return next();
+    //         },
+    //         ssl: {
+    //             require: true,
+    //             rejectUnauthorized: false,
+    //         },
+    //     },
+    //     timezone: 'Asia/Ho_Chi_Minh'
+    // }
+);
